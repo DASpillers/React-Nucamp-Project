2737f7d45db4ae3752c393d4f85ba335c89b2189
Daniel Spillers <spillersdaniela@gmail.com>
2021-09-01 14:12:24 -0400

    Adding to the SPA ('single page application')/ Learned to route to different pages

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
-// import CampsiteInfo from './CampsiteInfoComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
+// import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite ={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion ={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner ={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         };
 
@@ -28,6 +39,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
