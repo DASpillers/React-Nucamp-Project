822d1d55382e4eeea32b4650dd5ee6a5674898d6
Daniel Spillers <spillersdaniela@gmail.com>
2021-09-11 09:11:53 -0400

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 // import { CAMPSITES } from './shared/campsites';
 
+const store = ConfigureStore();
+
 class App extends Component {
+    
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
