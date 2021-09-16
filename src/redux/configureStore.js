93f58d37bf3e902952668eaace584142e7f41424
Daniel Spillers <spillersdaniela@gmail.com>
2021-09-16 13:20:03 -0400

    Fetch Handling Errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,15 +5,20 @@ import { Partners } from './partners';
 import {Promotions} from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers ({
+        combineReducers({
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
