b832105d252d1b68e24d06d268073746693f72b6
Daniel Spillers <spillersdaniela@gmail.com>
2021-09-16 13:39:37 -0400

    React Animations Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from "../shared/baseUrl";
+import { baseUrl } from '../shared/baseUrl';
 import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = val => val && val.length;
@@ -12,44 +12,54 @@ const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
     return (
-        <div className="col-md-5 m-1">
-            <FadeTransform
-                in
-                transformProps={{
-                    exitTransform: 'scale(0.5) translateY(-50%)'
-                }}>
-                <Card>
-                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </FadeTransform>
-        </div>
-        );
+      <div className="col-md-5 m-1">
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: "scale(0.5) translateY(-50%)",
+          }}
+        >
+          <Card>
+            <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+            <CardBody>
+              <CardText>{campsite.description}</CardText>
+            </CardBody>
+          </Card>
+        </FadeTransform>
+      </div>
+    );
 }
 
 function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return (
-            <div className="col-md-5 m-1">
-                <h4>Comments</h4>
-                <Stagger in>
-                    {
-                        comments.map(comment => {
-                            return (
-                                <Fade in key={comment.id}>
-                                    <div>
-                                        <p>
-                                            {comment.text}<br />
-                                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                        </p>
-                                    </div>
-                                </Fade>
-                            );
-                        })
-                    }
-                </Stagger>
+          <div className="col-md-5 m-1">
+            <h4>Comments</h4>
+            <Stagger in>
+              {comments.map((comment) => {
+                return (
+                  <Fade in key={comment.id}>
+                    <div>
+                      <p>
+                        {comment.text}
+                        <br />
+                        -- {comment.author},{" "}
+                        {new Intl.DateTimeFormat("en-US", {
+                          year: "numeric",
+                          month: "short",
+                          day: "2-digit",
+                        }).format(new Date(Date.parse(comment.date)))}
+                      </p>
+                    </div>
+                  </Fade>
+                );
+              })}
+            </Stagger>
+            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+          </div>
+        );
+    }
+
 }
 
 
